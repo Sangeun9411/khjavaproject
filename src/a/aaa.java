900a1b05eb40e8856f39824ea3728233f80bf351
user1 <user1@DESKTOP-MPUKNTA>
2022-11-25 16:53:08 +0900

    ADD: documents upload

---
+++ src/a/aaa.java
@@ -0,0 +1,19 @@
+package a;
+
+public class aaa {
+	/*
+	 
+	줌 : 282 782 3483 , pw: 220613
+	
+	github 토큰 : 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
+
+	파일서버 : http://sonof.iptime.org:8123
+
+	커리큘럼 : https://wealthy-quiet-986.notion.site/a5f378bc10b5460f9b47fbc13449b506
+
+	자습 : https://docs.google.com/spreadsheets/d/1FczIOY0FEsi1EaTsx-sz2uBtwLv-L5lk2CPMY26M4qw/edit?usp=sharing  
+
+	java 문제 db : https://wealthy-quiet-986.notion.site/e8c7750ad2d9455a81b265401791bf2d?v=424afbb536b045d0bb80b0ce61a98207
+	
+	*/
+}
